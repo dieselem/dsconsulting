5727ab9cef9beeb892af4f4d1c7705a5513c21a3
Diego Selem <diese@MacBook-Air-2.local>
2026-05-31 18:26:36 -0300

    test version marker

--- proxy-server.cjs
+++ proxy-server.cjs
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json({ limit: '2mb' }));
 
 app.get('/', (req, res) => {
-  res.send('Proxy OK');
+  res.send('Proxy OK v2');
 });
 
 app.post('/api/chat', async (req, res) => {
